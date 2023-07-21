4253583825f28fc54743552aae254aff7383571c
jimini <jimini@amazon.com>
2023-07-21 16:38:30 -0700

    init.sh fix

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+#. ~/.nvm/nvm.sh
 nvm install 16
-node -e "console.log('Running Node.js ' + process.version)"
-cd buy-with-prime-oauth-handler-ec2
+#node -e "console.log('Running Node.js ' + process.version)"
 npm i
-nohup node ~/buy-with-prime-oauth-handler-ec2/app.js > app.log &
+nvm use 16
+# nohup node ~/buy-with-prime-oauth-handler-ec2/app.js > app.log &
